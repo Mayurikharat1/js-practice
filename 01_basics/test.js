3c819a511b2b327ccf654cdf11c35a0746dccb15
Mayurikharat1 <147341621+Mayurikharat1@users.noreply.github.com>
2024-09-29 19:26:38 +0000

    Day2 change

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,10 @@
-console.log("Hello")
+//var let const in JS
+
+var accountCity="Raigad"
+let accountEmail="Mayuri@gmail.com";
+const accountId=12345678
+accountState="Jaipur"
+
+console.log(accountId);
+//or
+console.table([accountCity,accountEmail,accountId,accountState])
