3973836c2408774e108a62846c66b34e952785ed
Mayurikharat1 <147341621+Mayurikharat1@users.noreply.github.com>
2024-09-30 16:52:51 +0000

    datatype

---
+++ 01_basics/conversionOf.js
@@ -0,0 +1,29 @@
+//Conversion into number
+
+let num=33
+let num1="33"
+let num2="33abcd"
+let num3=null
+let num4=undefined
+let num5=true
+
+let covertInNum=Number (num)
+let covertInNum1=Number (num1)
+let covertInNum2=Number (num2)
+let covertInNum3=Number (num3)
+let covertInNum4=Number (num4)
+let covertInNum5=Number (num5)
+
+console.log(typeof covertInNum);
+console.log(typeof covertInNum1);
+console.log(typeof covertInNum2);
+console.log(typeof covertInNum3);
+console.log(typeof covertInNum4);
+console.log(typeof covertInNum5);
+
+console.log(covertInNum);
+console.log(covertInNum1);
+console.log(covertInNum2);
+console.log(covertInNum3);
+console.log(covertInNum4);
+console.log(covertInNum5);
---
+++ 01_basics/dataType.js
@@ -0,0 +1,15 @@
+"use strict"; //treat all js code as newer version
+
+let num=33
+let text="mayuri"
+let number="33abcd"
+let boolean=true
+let state;
+let state1=null
+
+console.log(typeof num);
+console.log(typeof text)
+console.log(typeof number)
+console.log(typeof boolean)
+console.log(typeof state)  //undefined
+console.log(typeof state1)      //null type is considered as -> object
