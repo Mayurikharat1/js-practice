c7c39b0c2066cd4e2150c5940a65c6ef9678ac77
Mayurikharat1 <147341621+Mayurikharat1@users.noreply.github.com>
2024-09-30 17:45:22 +0000

    conversion done

--- 01_basics/conversionOf.js
+++ 01_basics/conversionOf.js
@@ -14,16 +14,47 @@ let covertInNum3=Number (num3)
 let covertInNum4=Number (num4)
 let covertInNum5=Number (num5)
 
-console.log(typeof covertInNum);
-console.log(typeof covertInNum1);
+console.log(typeof covertInNum)
+console.log(typeof covertInNum1)
 console.log(typeof covertInNum2);
 console.log(typeof covertInNum3);
 console.log(typeof covertInNum4);
-console.log(typeof covertInNum5);
+console.log(typeof covertInNum5)
 
 console.log(covertInNum);
 console.log(covertInNum1);
-console.log(covertInNum2);
-console.log(covertInNum3);
+console.log(covertInNum2)
+console.log(covertInNum3)
 console.log(covertInNum4);
 console.log(covertInNum5);
+
+
+//Conversion into boolean
+
+let bool=1
+let bool1=0
+let bool2=""
+let bool3="Mayu"
+
+let covertInBool=Boolean (bool)
+let covertInBool1=Boolean (bool1)
+let covertInBool2=Boolean (bool2)
+let covertInBool3=Boolean (bool3)
+
+console.log(typeof covertInBool);
+console.log(typeof covertInBool1)
+console.log(typeof covertInBool2);
+console.log(typeof covertInBool3)
+
+console.log(covertInBool)
+console.log(covertInBool1)
+console.log(covertInBool2)
+console.log(covertInBool3)
+
+
+//Conversion into String
+
+let Num=45
+let covertInString=String (Num)
+console.log(typeof covertInString);
+console.log(covertInString)
