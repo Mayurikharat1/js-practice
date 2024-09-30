b7c9b4a2b99bf836f6f6cd114cde670f51adfaca
Mayurikharat1 <147341621+Mayurikharat1@users.noreply.github.com>
2024-09-30 16:24:18 +0000

    variable changes

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,10 +1,1 @@
-//var let const in JS
-
-var accountCity="Raigad"
-let accountEmail="Mayuri@gmail.com";
-const accountId=12345678
-accountState="Jaipur"
-
-console.log(accountId);
-//or
-console.table([accountCity,accountEmail,accountId,accountState])
+console.log("Hello");
