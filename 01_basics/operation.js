28febe5933b646379981895a48f3f970da197527
Mayurikharat1 <147341621+Mayurikharat1@users.noreply.github.com>
2024-10-01 15:40:48 +0000

    operation

---
+++ 01_basics/operation.js
@@ -0,0 +1,38 @@
+//operations
+
+let num=3
+let negNum=-num
+console.log(negNum);
+
+//operations:
+
+// 1.console.log(2+2);
+// 2.console.log(5-2);
+// 3.console.log(2*2);
+// 4.console.log(2**3);   //double multiplication is used to represent power of numer
+// 5.console.log(15/3);   //it takes quotient as output
+// 6.console.log(15%4);   //it takes remainder as output
+
+let str1 = "Hello"
+let str2 = " Bruno"
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2);  //12
+console.log(1 + "2");  //12
+console.log("1" + 2 + 3);   //123
+console.log(1 + 2 + "3");   //33
+console.log(1 + "2" + 3);   //123
+
+console.log(true);  //true
+console.log(+ true);  //1
+//console.log(true+);  //CTE
+console.log(+"");  //0
+
+let operator=35
+operator++;
+console.log(operator); //36
+
+let operator1=60
+++operator1;
+console.log(operator1); //61
